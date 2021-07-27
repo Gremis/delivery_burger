73899b625cfe3368b6545a346bce52047cbe2d04
Gremis Tovar <18993990tovargremis@gmail.com>
2021-07-26 21:03:56 -0300

    header and footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div><p>Footer</p></div>
+  <footer id="footer"><p>Make Your Burger &copy; 2021</p></footer>
 </template>
 <script>
 export default {
@@ -7,4 +7,15 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+#footer {
+  height: 200px;
+  background-color: #222;
+  border-top: 4px solid #111;
+  color: #fcba03;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+}
+</style>
